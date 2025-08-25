435ad6d579d14050ce151d9cf20e463c35190f34
lggyx <19546328912@163.com>
2025-08-25 08:54:13 +0800

    添加数组相关八股

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 ## Java学习Demo
+
+### JVM,JRE,JDK
+
+> JDK包括环境和执行工具
+>
+> JRE运行环境
+>
+> JVM执行程序
+
+### switch
+
+```java
+switch(str){
+    case "str1":
+        break;
+    case "str2":
+        break;
+    default:
+        break;
+}
+```
+
+### 数组
+
+Java的数组是一个固定长度的对象，跟C#一样，原数组没办法扩容，只能新建然后复制，但是可以使用系统自带的复制方法（效率未知）。
+
+两种方法：
+
+- System.arraycopy（最高效，适合大数组）
+- Arrays.copyOf（代码更简洁，底层同样高效）
+
+```java
+import java.util.Arrays;
+String[] oldArray = {"Hello", "World", "Java"};
+String[] newArray = new String[oldArray.length + 2];
+System.arraycopy(oldArray, 0, newArray, 0, oldArray.length);
+// newArray 后面可以继续添加元素
+```
+
+```java
+import java.util.Arrays;
+String[] oldArray = {"Hello", "World", "Java"};
+String[] newArray = Arrays.copyOf(oldArray, oldArray.length + 2);
+```
+
+<a color="red">如果频繁扩容，建议用 ArrayList，自动扩容且效率高，避免频繁手动复制数组</a>
+
+`八股文`:组是Java中用于存储相同类型元素的容器。通过索引可以访问和操作数组中的 元素。使用数组可以方便地组织和处理大量相同类型的数据
+
+个人评价：不如Python数组
+
+### 多维数组
+
+本质是数组的数组，时间复杂度会随着维度升高迭代
+
+多维数组是由多个⼀维数组组成的数据结构，用于表示表格、矩阵等具有多行多
+列的数据。通过多个索引可以访问和操作多维数组中的元素。多维数组的⻓度在创建时需要
+指定，并且每个维度的索引范围从0到长度减1。
+
--- demo_数组.java
+++ demo_数组.java
@@ -1,31 +1,42 @@
-public class demo_数组{
+import java.util.Arrays;
+
+public class demo_数组 {
     public static void main(String[] args) {
-        /**Integer[] array = {1, 2, 3, 4, 5};
-        for (Integer num : array) {
-            switch (num) {
-                case 1:
-                    System.out.println("Number is 1");
-                    break;
-                case 2:
-                    System.out.println("Number is 2");
-                    break;
-                case 3:
-                    System.out.println("Number is 3");
-                    break;
-                default:
-                    System.out.println("Number is not 1, 2, or 3 is: " + num);
-                    break;
-            }
-        }**/
+        /**
+         * Integer[] array = {1, 2, 3, 4, 5};
+         * for (Integer num : array) {
+         * switch (num) {
+         * case 1:
+         * System.out.println("Number is 1");
+         * break;
+         * case 2:
+         * System.out.println("Number is 2");
+         * break;
+         * case 3:
+         * System.out.println("Number is 3");
+         * break;
+         * default:
+         * System.out.println("Number is not 1, 2, or 3 is: " + num);
+         * break;
+         * }
+         * }
+         **/
         // String[] stringArray = {"Hello", "World", "Java"};
         // for (String str : stringArray) {
-        //     System.out.println("String is: " + str);
+        // System.out.println("String is: " + str);
         // }
-        String[] stringArray2;
-        stringArray2 = new String[5];
-        stringArray2[1]="Hello";
-        for(String str:stringArray2){
-            System.out.println("String2 is: "+str);
-        }
+        // String[] stringArray2;
+        // stringArray2 = new String[5];
+        // stringArray2[1]="Hello";
+        // for(String str:stringArray2){
+        // System.out.println("String2 is: "+str);
+        // }
+        String[] oldArray = { "Hello", "World", "Java" };
+        String[] newArray = new String[oldArray.length + 2];
+        System.arraycopy(oldArray, 0, newArray, 0, oldArray.length);
+        newArray[oldArray.length] = "Hello1";
+        newArray[oldArray.length + 1] = "World2";
+        System.out.println("New Array: " + Arrays.toString(newArray));
+        System.out.println("Old Array: " + Arrays.toString(oldArray));
     }
 }
