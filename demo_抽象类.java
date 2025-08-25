57780b723af4a1ac9c50b9d72fc9dfd27dcc86af
lggyx <19546328912@163.com>
2025-08-25 15:10:57 +0800

    添加抽象

---
+++ demo_抽象类.java
@@ -0,0 +1,28 @@
+public class demo_抽象类 {
+
+    public static void main(String[] args) {
+        Person p1 = new Student();
+        p1.run();
+        p1 = new Teacher();
+        p1.run();
+    }
+
+}
+
+abstract class Person {
+    public abstract void run();
+}
+
+class Student extends Person {
+    @Override
+    public void run() {
+        System.out.println("Student.run");
+    }
+}
+
+class Teacher extends Person {
+    @Override
+    public void run() {
+        System.out.println("Teacher.run");
+    }
+}
