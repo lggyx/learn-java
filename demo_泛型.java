30083451059470b3256778d04b465ca267c0b21e
lggyx <19546328912@163.com>
2025-08-27 09:57:18 +0800

    泛型

--- demo_泛型.java
+++ demo_泛型.java
@@ -1,14 +1,35 @@
 import java.util.ArrayList;
+import java.util.List;
 
-/**
- */
 public class demo_泛型 {
-    ArrayList<String> list = new ArrayList<>();
-    
+    static ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
-        demo_泛型 d=new demo_泛型();
-        d.list.add("hello");
-        d.list.add("world");
-        System.out.println(d.list.toString());
+        list.add("hello");
+        list.add("world");
+        System.out.println(list.toString());
+
+        demo_泛型 demo = new demo_泛型();
+        demo.show(list);
+        System.out.println(demo.get(0));
+        System.out.println(demo.getAll());
+        System.out.println(demo.getAll1());
+    }
+
+    void show(ArrayList<String> list) {
+        for (String string : list) {
+            System.out.println(string);
+        }
+    }
+
+    String get(int index) {
+        return list.get(index);
+    }
+    List<String> getAll() {
+        return list;
+    }
+
+    List<?> getAll1() {
+        return list;
     }
 }
