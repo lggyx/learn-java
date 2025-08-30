227ab15d9935dad7930ddc967a296a9a0b863390
lggyx <19546328912@163.com>
2025-08-30 16:38:22 +0800

    接口测试正常

--- lggyx_javaweb_demo/src/main/java/com/lggyx/controller/DeptController.java
+++ lggyx_javaweb_demo/src/main/java/com/lggyx/controller/DeptController.java
@@ -5,6 +5,7 @@ import com.lggyx.pojo.Result;
 import com.lggyx.service.DeptService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+    @GetMapping("/depts")
     public Result list() {
         log.info("部门列表数据查询");
         List<Dept> list = deptService.list();
